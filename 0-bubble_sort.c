8ced20cb99998ab947bded89c0d85179e39c8f10
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-01-23 17:21:37 -0800

    Buble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,54 +1,28 @@
 #include "sort.h"
+
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * @array: Array
- * @size: Pointer to the previous element of the list
- *
- * Description: Sorts an array of integers in
- * ascending order using the Bubble sort algorithm
- * Return: Void
+ *bubble_sort - sorts an array of integers in ascending orde
+ *@array: the array to be sort
+ *@size: the size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0;
-	int aux;
+	size_t j, i;
+	int n;
 
-	if (!array || !size)
+	if (size < 2)
 		return;
-	while (unsorted(array, size))
+	for (i = 0; i < size - 1; i++)
 	{
-		for (i = 0; i < size && array[i + 1]; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				aux = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = aux;
+				n = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = n;
 				print_array(array, size);
-				if (!unsorted(&array[i + 1], size))
-					break;
 			}
 		}
 	}
-}
-
-/**
- * unsorted - Verify if an array is unsorted
- * @array: Array
- * @size: Size
- *
- * Description: Verify if an array is unsorted
- * Return: 1 if it's unsorted 0 otherwise
- */
-int unsorted(int *array, size_t size)
-{
-	size_t j = 0;
-
-	while (j < size - 1)
-	{
-		if (array[j] > array[j + 1])
-			return (1);
-		j++;
-	}
-	return (0);
 }
